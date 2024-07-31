dc54819955f76a7db551a55bb1cd1fe0d685118e
dexterarmy <immohitsharma8@gmail.com>
2024-07-31 13:41:57 +0530

    first commit

---
+++ practise/Main.java
@@ -0,0 +1,21 @@
+
+import java.util.Arrays;
+import java.util.List;
+
+class Main {
+    
+    public static void main(String[] args) throws Exception
+    {
+
+
+        List<Integer> nums = Arrays.asList(1,2,3,4);
+
+        System.out.print(nums);
+
+
+        
+    }
+      
+}
+
+
